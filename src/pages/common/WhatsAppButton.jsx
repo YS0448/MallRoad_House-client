b9cc579d36322ea7540bb62135bf532c399f1b1e
YS0448 <yogeshsingh0448@gmail.com>
2025-08-14 20:41:51 +0530

    changes in whatsapp button

--- src/pages/common/WhatsAppButton.jsx
+++ src/pages/common/WhatsAppButton.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import "../../assets/styles/components/common/WhatsAppButton.css";
 
+const whatsappNumber = import.meta.env.VITE_WHATSAPP_NUMBER;
+const textMessage = "Hi%2C%20I%20want%20to%20reserve%20a%20table";    
 const WhatsAppButton = () => (
   <a
-    href="https://wa.me/918448815462?text=Hi%2C%20I%20want%20to%20reserve%20a%20table"
+    href={`https://wa.me/${whatsappNumber}?text=${textMessage}`}
     className="whatsapp-float"
     target="_blank"
     rel="noopener noreferrer"
